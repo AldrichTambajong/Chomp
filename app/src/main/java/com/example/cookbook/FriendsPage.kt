8787b5281749199a8e2e14fc7bbd2ca662b7b5f9
Aldrich Tambajong <aldricht9@gmail.com>
2022-11-03 23:04:13 -0400

    Added Profile Page for Friends

---
+++ app/src/main/java/com/example/cookbook/FriendsPage.kt
@@ -0,0 +1,13 @@
+package com.example.cookbook
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cookbook.databinding.ActivitySignInBinding
+
+class FriendsPage: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friends_page)
+    }
+}
