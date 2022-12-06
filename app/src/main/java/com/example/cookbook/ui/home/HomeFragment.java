ce85b7229815ed9371d13d22749adc478ce70044
Aldrich Tambajong <aldricht9@gmail.com>
2022-12-06 14:23:54 -0500

    implemented documents in Recipes collection for recyclerView

--- app/src/main/java/com/example/cookbook/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/cookbook/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.cookbook.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +15,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cookbook.R;
 import com.example.cookbook.databinding.FragmentHomeBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private RecyclerView rv;
-    private String[] names = {"Chicken Parm", "Fettucine Alfredo", "ThumbPrint Cookie"};
-    private Integer[] images = {R.drawable.chicken_parm,R.drawable.fettuccine_alfredo,R.drawable.thumbprint_cookie};
+    private ArrayList<String> names = new ArrayList<String>();
+    private ArrayList<String> images = new ArrayList<String>();
+    private FirebaseFirestore firestoreDb = FirebaseFirestore.getInstance();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -31,9 +40,29 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
         rv = binding.firestorelist;
 
-        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
-        rv.setLayoutManager(llm);
-        rv.setAdapter(new RecipesRecyclerViewHolder(names,images,getActivity()));
+        if(names.size() == 0){
+            firestoreDb.collection("Recipes").get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if(task.isSuccessful()){
+                                for(QueryDocumentSnapshot doc : task.getResult()){
+                                    names.add(doc.getId());
+                                    images.add(doc.get("url").toString());
+                                    Log.d("TAG",doc.getId());
+                                }
+                                LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+                                rv.setLayoutManager(llm);
+                                rv.setAdapter(new RecipesRecyclerViewHolder(names,images,getActivity()));
+                            }
+                        }
+                    });
+        }
+        else{
+            LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+            rv.setLayoutManager(llm);
+            rv.setAdapter(new RecipesRecyclerViewHolder(names,images,getActivity()));
+        }
 
         return root;
     }
