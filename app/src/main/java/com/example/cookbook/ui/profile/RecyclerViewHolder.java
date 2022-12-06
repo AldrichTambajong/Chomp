8668e30ef6def8360c63a2bc1040ed688f4b74e1
Aldrich Tambajong <aldricht9@gmail.com>
2022-12-06 14:25:10 -0500

    added glide to load images

--- app/src/main/java/com/example/cookbook/ui/profile/RecyclerViewHolder.java
+++ app/src/main/java/com/example/cookbook/ui/profile/RecyclerViewHolder.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.cookbook.R;
 import com.squareup.picasso.Picasso;
 
@@ -36,6 +37,9 @@ public class RecyclerViewHolder extends RecyclerView.Adapter<RecyclerViewHolder.
     @Override
     public void onBindViewHolder(@NonNull Item holder, int position) {
         holder.name.setText(names.get(position));
+        Glide.with(context)
+                .load(R.drawable.logo)
+                .into(holder.link);
     }
 
     @Override
